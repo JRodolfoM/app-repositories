82b659827da3145f77c32be31fc7c891d2849e09
JRodolfoM <j_rodolfom@live.com>
2022-08-07 20:55:49 -0300

    Finalizando App Do Bootcamp

--- app/src/main/java/br/com/jrmantovani/apprepositories/ui/MainActivity.kt
+++ app/src/main/java/br/com/jrmantovani/apprepositories/ui/MainActivity.kt
@@ -6,10 +6,18 @@ import android.util.Log
 import android.view.Menu
 import androidx.appcompat.widget.SearchView
 import br.com.jrmantovani.apprepositories.R
+import br.com.jrmantovani.apprepositories.core.createDialog
+import br.com.jrmantovani.apprepositories.core.createProgressDialog
+import br.com.jrmantovani.apprepositories.core.hideSoftKeyboard
 import br.com.jrmantovani.apprepositories.databinding.ActivityMainBinding
+import br.com.jrmantovani.apprepositories.presentation.MainViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
 class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
+    private val dialog by lazy { createProgressDialog() }
+    private val viewModel by viewModel<MainViewModel>()
+    private val adapter by lazy { RepoListAdapter() }
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +25,24 @@ class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
+
+        binding.rvRepos.adapter = adapter
+
+        viewModel.repos.observe(this) {
+            when (it) {
+                MainViewModel.State.Loading -> dialog.show()
+                is MainViewModel.State.Error -> {
+                    createDialog {
+                        setMessage(it.error.message)
+                    }.show()
+                    dialog.dismiss()
+                }
+                is MainViewModel.State.Success -> {
+                    dialog.dismiss()
+                    adapter.submitList(it.list)
+                }
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -28,7 +54,8 @@ class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
     }
 
     override fun onQueryTextSubmit(query: String?): Boolean {
-        Log.e(TAG,"onQueryTextSubimit : $query")
+        query?.let { viewModel.getRepoList(it) }
+        binding.root.hideSoftKeyboard()
         return true
     }
 
