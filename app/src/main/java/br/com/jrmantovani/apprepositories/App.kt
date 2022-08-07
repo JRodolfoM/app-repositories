50213174f3e832a3c4050c073523b08eb9e4b059
JRodolfoM <j_rodolfom@live.com>
2022-08-07 20:30:12 -0300

    criando o view model

--- app/src/main/java/br/com/jrmantovani/apprepositories/App.kt
+++ app/src/main/java/br/com/jrmantovani/apprepositories/App.kt
@@ -3,6 +3,7 @@ package br.com.jrmantovani.apprepositories
 import android.app.Application
 import br.com.jrmantovani.apprepositories.data.di.DataModule
 import br.com.jrmantovani.apprepositories.domain.di.DomainModule
+import br.com.jrmantovani.apprepositories.presentation.di.PresentationModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -16,7 +17,7 @@ class App : Application() {
         }
         DataModule.load()
         DomainModule.load()
-        //PresentationModule.load()
+        PresentationModule.load()
 
     }
 }
